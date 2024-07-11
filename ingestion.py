ec4d217921b4fdd583f007f5dbd1982a784fb62f
davidhyun <hyunh317@gmail.com>
2024-07-12 00:33:13 +0900

    Load document, splitting, embedding, storing in vectordb(pinecone)

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,27 @@
-from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv; load_dotenv()
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
-
-load_dotenv()
 
 if __name__ == "__main__":
     # Load -> Split -> Embed -> Store
     
-    print("INGESTION")
+    print("Ingesting...")
+    loader = TextLoader("./mediumblog1.txt")
+    document = loader.load()
+    
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+    
+    print("Embedding...")
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    
+    print("Storing...")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+    
+    print("Done")
